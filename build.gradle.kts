1b75d6aca4d133b4561c58dd830c13efae6886f4
Nikolai Matveev <nmatweew@yandex.ru>
2023-09-22 09:08:58 +0300

    Add dependency version control plugin to gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,4 +8,19 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "1.8.10" apply false
 
     id("androidx.navigation.safeargs") version Versions.navigation apply false
+
+    id("com.github.ben-manes.versions") version "0.48.0"
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
+    rejectVersionIf {
+        isNonStable(candidate.version)
+    }
 }
